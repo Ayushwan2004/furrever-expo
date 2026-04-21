d3c21adb552f04f9c96cf8288c25ea68e114c6bd
Ayush Wankhede <ayushwankhede9@gmail.com>
2026-04-21 12:24:54 +0530

    fix: config plugin syntax

--- plugins/withAndroidManifest.js
+++ plugins/withAndroidManifest.js
@@ -1,15 +1,21 @@
 const { withAndroidManifest } = require('@expo/config-plugins');
 
-module.exports = withAndroidManifest(config, async (config) => {
-  const mainApplication = config.modResults.manifest.application[0];
-  
-  const metaDataList = mainApplication['meta-data'] || [];
-  
-  metaDataList.forEach((metaData) => {
-    if (metaData.$['android:name'] === 'com.google.firebase.messaging.default_notification_color') {
-      metaData.$['tools:replace'] = 'android:resource';
-    }
-  });
+const withFCMColor = (config) => {
+  return withAndroidManifest(config, async (config) => {
+    const mainApplication = config.modResults.manifest.application[0];
+    const metaDataList = mainApplication['meta-data'] || [];
 
-  return config;
-});
+    metaDataList.forEach((metaData) => {
+      if (
+        metaData.$['android:name'] ===
+        'com.google.firebase.messaging.default_notification_color'
+      ) {
+        metaData.$['tools:replace'] = 'android:resource';
+      }
+    });
+
+    return config;
+  });
+};
+
+module.exports = withFCMColor;
